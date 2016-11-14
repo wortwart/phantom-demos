1d038f1fbbc12beced5ae5b5b17c45b44a35d5d1
Herbert Braun <wortwart@woerter.de>
2016-11-14 10:22:40 +0100

    Delete duplicates

--- heise_einstieg.js
+++
@@ -1,9 +0,0 @@
-var page = require('webpage').create();
-page.open('http://www.heise.de/', function(status) {
-	if (status !== 'success') {
-		console.log('Netzwerkproblem: ' + status);
-		phantom.exit();
-	}
-	console.log(page.plainText);
-	phantom.exit();
-});
